255cf1791334d38c4b4b01d0705a96c3cd2b3880
Francesco Canessa <makevoid@gmail.com>
2013-08-20 11:54:09 +0200

    render terms, style tidbits

--- app/terms.py
+++ app/terms.py
@@ -1,4 +1,18 @@
+from jinja2 import Template
+from terms_list import terms
+
 def run(web):
-  # TODO: extract this in fuqit
-  path = "/".join(web.path.split("/")[2:])
-  return "terms... "+path
+  # TODO: extract these in fuqit
+  title_url = "/".join(web.path.split("/")[2:])
+
+  term = {}
+  for term2 in terms:
+    if term2["title_url"] == title_url:
+      term = term2
+
+  vars = { "term": term }
+  template = web['app'].env.get_template('term.html')
+
+  content = template.render(vars)
+
+  return content
