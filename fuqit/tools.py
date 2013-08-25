a4f0b6f2378f3ed3f3883982e44c9c539369c335
Francesco Canessa <makevoid@gmail.com>
2013-08-25 15:50:35 +0200

    updated fuqit

--- fuqit/tools.py
+++ fuqit/tools.py
@@ -1,16 +1,16 @@
 # Fuqit Web Framework
 # Copyright (C) 2013  Zed A. Shaw
-# 
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU Affero General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -23,41 +23,41 @@ __all__ = ['module']
 mimetypes.init()
 
 def module(name):
-  themodule = import_module("." + name, package="app")
-  return themodule
+    themodule = import_module("." + name, package="app")
+    return themodule
 
 def build_context(params, app, handler):
-  variables = {'web': 
-         {'params': params,
-          'headers': handler.headers,
-          'path': handler.path,
-          'method': handler.command,
-          'client_address': handler.client_address,
-          'request_version': handler.request_version,
-          'app': app
-         },
-         'module': module
-        }
-  return variables
+    variables = {'web':
+                 {'params': params,
+                  'headers': handler.headers,
+                  'path': handler.path,
+                  'method': handler.command,
+                  'client_address': handler.client_address,
+                  'request_version': handler.request_version,
+                  'app': app,
+                 },
+                 'module': module,
+                }
+    return variables
 
 def parse_request(path, request_body):
-  request_params = {}
+    request_params = {}
 
-  if '?' in path:
-    path, params = path.split('?', 1)
-    params = cgi.parse_qsl(params)
-    request_params.update(params)
+    if '?' in path:
+        path, params = path.split('?', 1)
+        params = cgi.parse_qsl(params)
+        request_params.update(params)
 
-  if request_body:
-    params = cgi.parse_qsl(request_body)
-    request_params.update(params)
+    if request_body:
+        params = cgi.parse_qsl(request_body)
+        request_params.update(params)
 
-  return path, request_params
+    return path, request_params
 
 
 def make_ctype(ext, default_mtype):
-  mtype = mimetypes.types_map.get(ext, default_mtype)
-  return {'Content-Type': mtype}
+    mtype = mimetypes.types_map.get(ext, default_mtype)
+    return {'Content-Type': mtype}
 
 
 
